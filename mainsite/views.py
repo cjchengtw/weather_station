4c4b7f6167524a56bb39da3b79f4e6b0420d1f7e
Jonathan <jonathan950400@gmail.com>
2016-12-22 23:30:59 +0800

    weather data upload

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.template.loader import get_template
+from django.http import HttpResponse
+from urllib.request import urlopen
+import json
 # Create your views here.
+def homepage(request):
+	template = get_template('index.html')
+	data = urlopen("https://api.thingspeak.com/channels/202242/fields/1.json?key=YTCKLSZDCL10P60Q&results=1")\
+	.read().decode('utf-8')
+	dataJson = json.loads(data)
+	meta = dataJson.get("feeds")[0]['field1']
+	html = template.render(locals())
+	return HttpResponse(html)
