50949754e768aea8d24c3c54f63354e720338420
Jonathan <jonathan950400@gmail.com>
2017-01-01 16:29:40 +0800

    bug - status page

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 from django.template.loader import get_template
 from django.http import HttpResponse
+from bokeh.resources import CDN
 from urllib.request import urlopen
 import json
+from bokeh.plotting import figure, output_file, show 
+from bokeh.embed import components
 # Create your views here.
 def homepage(request):
     template = get_template('index.html')
-    data = urlopen("http://api.thingspeak.com/channels/148353/feed/last.json?key=K8TNQ7BOCQ3JZMK2").read().decode('utf-8')
-    dataJson = json.loads(data)
+    single_data = urlopen("http://api.thingspeak.com/channels/148353/feed/last.json?key=K8TNQ7BOCQ3JZMK2&timezone=Asia/Taipei").read().decode('utf-8')
+    dataJson = json.loads(single_data)
     temperature  = dataJson.get('field1')
     wet  = dataJson.get('field2')
     ultraviolet_radiation = dataJson.get('field3')
     light_intensity = dataJson.get('field4')
     now = dataJson.get('created_at')
+    plot = figure()
+    plot.line([1,2,3,4,5],[5,4,3,2,1])
+    script, div = components(plot)
     html = template.render(locals())
     return HttpResponse(html)
 
-def static(request):
-    template = get_template('static.html')
+def status(request):
+    template = get_template('status.html')
+    return HttpResponse(template)
--- weather_station/urls.py
+++ weather_station/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from mainsite.views import homepage
+from mainsite.views import homepage,status
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^$', homepage),
+     url(r'^status/$', status),
+     
 ]
